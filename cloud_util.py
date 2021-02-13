520c6493314e2b9db46386e1e5da1f7207c92c1e
christopheryu <cyu95@stanford.edu>
2021-02-13 00:54:31 -0800

    added cloud util test file

---
+++ cloud_util.py
@@ -0,0 +1,88 @@
+import rasterio
+import boto3
+from rasterio.plot import show
+import matplotlib.pyplot as plt
+from orders import place_order, poll_for_success, download_order
+import requests
+from requests.auth import HTTPBasicAuth
+
+# Define global variables
+AWS_SERVER_PUBLIC_KEY = "AKIAI6BOY2NHNQF6MX3A"
+AWS_SERVER_SECRET_KEY = "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"
+
+"""
+Test function: This function performs a downloads a single product to the s3 bucket
+@Param: none
+@Return: none
+"""
+def test_write_to_s3():
+
+    # test input variables
+    orders_url = 'https://api.planet.com/compute/ops/orders/v2'
+    PLANET_API_KEY = 'b99bfe8b97d54205bccad513987bbc02'
+    auth = HTTPBasicAuth(PLANET_API_KEY, '')
+
+    # define product
+    single_product = [
+        {
+            "item_ids": ["20151119_025740_0c74"],  # ***NOTE*** Code to search for item id is already done
+            "item_type": "PSScene4Band",
+            "product_bundle": "analytic"
+        }
+    ]
+
+    # define delivery
+    delivery = {
+        "amazon_s3": {
+            "bucket": "cs230data",
+            "aws_region": "us-west-1",
+            "aws_access_key_id": AWS_SERVER_PUBLIC_KEY,
+            "aws_secret_access_key": AWS_SERVER_SECRET_KEY,
+            "path_prefix": "test"
+        }
+    }
+
+    # define request
+    request = {
+        "name": "test download",
+        "products": single_product,
+        "delivery": delivery
+    }
+
+    # make download to s3
+    order_url = place_order(request, auth)
+    poll_for_success(order_url, auth)
+    print("Download to AWS s3 was successful")
+
+"""
+Test function: This function attempts to open a .tif from the cloud and interact with the rgbn data and make a sample plot.
+# Note: This function assumes 'test_image1.tif' is in the root of the bucket
+@Param: aws session
+@Return: none
+"""
+def test_tif(session):
+    # Read in .tif test
+    with rasterio.Env(aws_secret_access_key=AWS_SERVER_SECRET_KEY,
+                      aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
+                      aws_session=session):
+        url = 's3://cs230data/test_image1.tif'
+        data = rasterio.open(url)
+        b, g, r, n = data.read()
+        plt.imshow(b)
+        plt.show()
+    print("printing tif was sucessful")
+
+
+if __name__ == "__main__":
+
+    # Define session for aws
+    session = boto3.Session(
+        aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
+        aws_secret_access_key=AWS_SERVER_SECRET_KEY,
+    )
+    s3 = session.resource('s3')
+    bucket = s3.Bucket('cs230data')
+
+    # Testing functions
+    test_write_to_s3()
+    # test_tif(session)
